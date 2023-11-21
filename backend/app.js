aa61bb5e7ac1d20d1adc72e811c4f6b91e81260e
Divya R <kwrdivyaraj100@gmail.com>
2023-11-21 12:19:16 +0530

    Checking the products

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require ('express');
 const cors = require ('cors');
+const productRouter = require('./routes/productRoute.js')
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(productRouter);
 
 
 app.listen(3300, ()=>{
