85b1d83b89e137044b0986ab56cf5f7656f8b3b0
Divya R <kwrdivyaraj100@gmail.com>
2023-11-25 13:03:56 +0530

    Importing header components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App(){
+    const [cartItems, setCartItems] = useState([]);
     return(
-        <div>Hello World</div>
+        <BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route exact path = '/' element = {<Products/>}/>
+            <Route exact path = '/signup' element = {<Signup/>}/>
+            <Route exact path = '/cart' element = {<Cart/>}/>
+        </Routes>
+        </BrowserRouter>
     );
 }
 
