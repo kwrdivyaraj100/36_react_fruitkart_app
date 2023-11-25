cae2e42143224b49c51759198f0f8d3ede447462
Divya R <kwrdivyaraj100@gmail.com>
2023-11-25 17:28:48 +0530

    Function for handleAddProduct, handleRemoveProduct and handleClearCart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,63 @@ import Products from './components/products/Products.jsx';
 import Signup from './components/signup/Signup.jsx';
 import Cart from './components/cart/Cart.jsx';
 import Header from './components/header/Header.jsx';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App(){
+function App() {
+
+
     const [cartItems, setCartItems] = useState([]);
-    return(
+    
+    function handleAddProduct(product) {
+        const productExist = cartItems.find(item => {
+            return item.id === product.id;
+        });
+
+        if (productExist) {
+            setCartItems(cartItems.map(item => {
+                return item.id === product.id ?
+                    { ...productExist, quantity: productExist.quantity + 1 } :
+                    item;
+            }))
+        }
+        else {
+            setCartItems([...cartItems, { ...product, quantity: 1 }]);
+        }
+    }
+
+    function handleRemoveProduct(product) {
+        const productExist = cartItems.find(item => {
+            return item.id === product.id;
+        });
+
+        if (productExist.quantity === 1) {
+            setCartItems(cartItems.filter(item => {
+                return item.id !== product.id;
+            }));
+        }
+        else {
+            setCartItems(cartItems.map(item => {
+                return item.id === product.id ?
+                    { ...productExist, quantity: productExist.quantity - 1 } :
+                    item
+            }))
+        }
+    }
+
+    function handleCartClearence() {
+        setCartItems([]);
+    }
+
+
+
+    return (
         <BrowserRouter>
-        <Header/>
-        <Routes>
-            <Route exact path = '/' element = {<Products/>}/>
-            <Route exact path = '/signup' element = {<Signup/>}/>
-            <Route exact path = '/cart' element = {<Cart/>}/>
-        </Routes>
+            <Header />
+            <Routes>
+                <Route exact path='/' element={<Products />} />
+                <Route exact path='/signup' element={<Signup />} />
+                <Route exact path='/cart' element={<Cart />} />
+            </Routes>
         </BrowserRouter>
     );
 }
